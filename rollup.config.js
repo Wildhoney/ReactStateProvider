298ee864d96bac23a024848de12e97c86122bf3e
Wildhoney <adam.timberlake@gmail.com>
2019-07-26 18:56:08 +0100

    Setup build :package: w/ Rollup

---
+++ rollup.config.js
@@ -0,0 +1,49 @@
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import babel from 'rollup-plugin-babel';
+import { terser } from 'rollup-plugin-terser';
+
+module.exports = {
+    input: 'src/index.js',
+    external: ['react', 'react-dom', 'prop-types'],
+    output: [
+        {
+            file: 'dist/rsp.cjs.js',
+            format: 'cjs',
+            sourcemap: true,
+            exports: 'named',
+        },
+        {
+            file: 'dist/rsp.esm.js',
+            format: 'esm',
+            sourcemap: true,
+            exports: 'named',
+        },
+    ],
+    plugins: [
+        resolve(),
+        babel({
+            exclude: 'node_modules/**',
+            runtimeHelpers: true,
+        }),
+        commonjs({
+            namedExports: {
+                include: 'node_modules/**',
+                'node_modules/react/index.js': [
+                    'createContext',
+                    'useContext',
+                    'useReducer',
+                ],
+                'node_modules/react-is/index.js': [
+                    'isElement',
+                    'isValidElementType',
+                    'ForwardRef',
+                ],
+                'node_modules/styled-components/dist/styled-components.esm.js': [
+                    'createContext',
+                ],
+            },
+        }),
+        terser(),
+    ],
+};
